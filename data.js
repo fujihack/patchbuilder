02fc8dc62f1a5c4f79e8ba04bd4c1613df76fc06
petabyt <petabyt@users.noreply.github.com>
2022-07-12 21:03:02 +0000

    Update binaries

--- data.js
+++ data.js
@@ -1,2 +1,1 @@
-// precompiled binary links
 [{"name": "xt10", "main": "", "jump": ""}, {"name": "xt4", "main": "", "jump": ""}, {"name": "hs20exr", "main": "", "jump": ""}, {"name": "z3", "main": "", "jump": ""}, {"name": "xf1", "main": "main-xf1.bin", "jump": "main-xf1.bin"}, {"name": "stub", "main": "", "jump": ""}]
